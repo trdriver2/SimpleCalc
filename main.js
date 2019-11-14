bfb8e46c2862053a86943e8d0c297f51f2a3bb46
tyler <tdriver@talentpath.com>
2019-11-14 13:32:01 -0600

    created the rpn function

--- main.js
+++ main.js
@@ -31,6 +31,35 @@ class Calculator {
     this.register = 0;
     return this;
   }
+  rpn(evaluate) {
+    evaluate = evaluate.split(" ");
+    const expressions = ["+", "-", "*", "/"];
+    for (let i = 0; i < evaluate.length; i++) {
+      if (isNaN(evaluate[i]) && expressions.includes(evaluate[i])) {
+        let exp = this.evaluateExpression(evaluate[i]).bind(this);
+
+        if (i === 2 && !isNaN(evaluate[i - 1]) && !isNaN(evaluate[i - 2])) {
+          this.register = parseInt(evaluate[i - 2]);
+          exp(parseInt(evaluate[i - 1]));
+        } else if (i - 1 >= 0 && !isNaN(evaluate[i - 1])) {
+          exp(parseInt(evaluate[i - 1]));
+        } else {
+          console.error("The provided string cannot be evaluated");
+          return null;
+        }
+      }
+    }
+    return this.register;
+  }
+  evaluateExpression(exp) {
+    if (exp === "+") return this.addition;
+    else if (exp === "-") return this.subtraction;
+    else if (exp === "*") return this.multiplication;
+    else if (exp === "/") return this.division;
+  }
 }
+
+let a = new Calculator(0);
+console.log(a.rpn("1 2 + 7 - 2 *"));
 
 module.exports = Calculator;
